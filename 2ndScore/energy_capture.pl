cee1b4208b4d5cb8a8da8cf12fc41404d673136d
DolapoA <dolaajayi@hotmail.com>
2017-08-30 18:50:39 +0100

    Add files via upload

---
+++ 2ndScore/energy_capture.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+use Data::Dumper;
+use Regexp::Common qw /number/;
+
+##Capture desired data
+my $HairpinData = qr/^\s*($RE{num}{real})\s+  # energy
+						 ($RE{num}{int})\s+ # start
+						 \.\.\s+
+						 ($RE{num}{int})\s+ # end
+						 \w+\s*
+						 [\w+|-]?\s+
+						 (\w+)\s*	 # spacer
+						 [\w+|-]?\s*
+/x;
+open my $file, '<', "filter_test.hairpin", or die $!;
+
+##Fill array with energy scores
+my @scores;
+while (<$file>){
+	if (my ($energy, $start, $end, $spacer) = /$HairpinData/){
+		push @scores, $energy;
+	}
+}
+
+#print Dumper(\@lengths);
+
+##Print energy scores to new file
+foreach my $element (@scores){
+	open my $new_file, '>>', "test_count.txt", or die $!;
+		print $new_file $element." ";
+		close $new_file;
+}
+close $file;
+
